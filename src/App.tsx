398af005e575404c09e28c9f0fe860ec1f4e0c83
Rakhmatullaev Abdulaziz <frrhyk@gmail.com>
2024-10-18 00:24:57 +0500

    update 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,11 @@ import Tasks from './pages/tasks';
 import Frens from './pages/frens';
 import Lrbd from './pages/lrbd';
 import Wallet from './pages/wallet';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+
+interface HapticFeedback {
+  impactOccurred(type: 'light' | 'medium' | 'heavy'): void;
+}
 
 // Define types for Telegram and WebApp
 interface TelegramWebApp {
@@ -14,6 +18,7 @@ interface TelegramWebApp {
   expand(): void;
   disableVerticalSwipes(): void;
   platform(): string;
+  HapticFeedback: HapticFeedback;
 }
 
 interface Telegram {
@@ -27,27 +32,42 @@ declare global {
   }
 }
 
-const ScrollToTop = () => {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-};
-
-function App() {
+export default function App() {
   const [loading, setLoading] = useState(true);
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     const webApp = window.Telegram.WebApp;
-    webApp.expand();
-    webApp.disableVerticalSwipes();
 
+    webApp.expand();
+   
     setTimeout(() => {
       setLoading(false);
-    }, 1200);
+    }, 500);
+    
+    webApp.disableVerticalSwipes();
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+
+  useEffect(() => {
+    // Button is displayed after scrolling for 500 pixels
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 20) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
+  return () => window.removeEventListener("scroll", toggleVisibility);
   }, []);
 
   return (
@@ -55,14 +75,14 @@ function App() {
       {loading ? (
         <div className='loadcol'>
           <svg className='spinner' height="100%" viewBox="0 0 32 32" width="100%">
-            <circle cx="16" cy="16" fill="none" r="14" stroke-width="4" style={{ stroke: 'rgb(29, 155, 240)', opacity: 0.2 }}></circle>
-            <circle cx="16" cy="16" fill="none" r="14" stroke-width="4" style={{ stroke: 'rgb(29, 155, 240)', strokeDasharray: 80, strokeDashoffset: 60 }}></circle>
+            <circle cx="16" cy="16" fill="none" r="14" strokeWidth="4" style={{ stroke: 'rgb(29, 155, 240)', opacity: 0.2 }}></circle>
+            <circle cx="16" cy="16" fill="none" r="14" strokeWidth="4" style={{ stroke: 'rgb(29, 155, 240)', strokeDasharray: 80, strokeDashoffset: 60 }}></circle>
           </svg>
+          v1.5
         </div>
       ) : (
         <div className='container'>
-          <div className="main">
-              <ScrollToTop />
+          <div className="main">   
               <div className="mainCon">
                 <Routes>
                   <Route path="x/tasks" element={<Tasks />} />
@@ -71,6 +91,11 @@ function App() {
                   <Route path="x/frens" element={<Frens />} />
                   <Route path="x/wallet" element={<Wallet />} />
                 </Routes>
+                <button id="toTop" className={isVisible ? "show" : ""} onClick={scrollToTop}>
+                  <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                      <path d="m13 19c0 .5523-.4477 1-1 1s-1-.4477-1-1v-11.58579l-4.29289 4.29289c-.39053.3905-1.02369.3905-1.41422 0-.39052-.3905-.39052-1.0237 0-1.4142l6.00001-6.00001c.3905-.39052 1.0237-.39052 1.4142 0l6 6.00001c.3905.3905.3905 1.0237 0 1.4142s-1.0237.3905-1.4142 0l-4.2929-4.29289z" fill="currentColor"/>
+                  </svg>
+                </button>
               </div>
             </div>
             <nav className="navbar">
@@ -80,6 +105,4 @@ function App() {
       )}
     </Router>
   );
-}
-
-export default App;
+};
